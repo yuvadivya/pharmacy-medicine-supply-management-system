a8a2f17a9c4b8013bab0295847e8b4154910a12f
yuvadivya <49725550+yuvadivya@users.noreply.github.com>
2021-04-12 17:10:27 +0530

    Delete medical-representative-schedule-service directory

--- medical-representative-schedule-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into medical_representatives (name, phone_number) values('Medical Representative 1', '9456481521');
-insert into medical_representatives (name, phone_number) values('Medical Representative 2', '9811548231');
-insert into medical_representatives (name, phone_number) values('Medical Representative 3', '9846874134');
